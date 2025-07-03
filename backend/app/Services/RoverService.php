bcdaa854dc05c277f0c399e0d13c7d896c6398df
pmunne <munnepau@gmail.com>
2025-07-03 10:46:46 +0200

    moveForward and obstacle

--- backend/app/Services/RoverService.php
+++ backend/app/Services/RoverService.php
@@ -1,6 +1,14 @@
 <?php
 namespace App\Services;
 
+/**
+ * Rover service
+ * 
+ * Rover service gestiona todos los movimientos del rover en una cuadricula de 200x200. 
+ *  'f' para avanzar, 'l' para girar a la izquierda, 'r' para girar a la derecha.
+ *  Detecta los obstaculos y detiene el rover. Vuelve a la ultima posicion valida.
+ *  
+ */
 class RoverService
 {
     private int $x;
@@ -26,5 +34,44 @@ class RoverService
         return [];
     }
 
-    public function move()
+    private function moveForward()
+    {
+        [$mx , $my] = match ($this->direction) {
+            'N' => [0, 1],
+            'E' => [1, 0],
+            'S' => [0, -1],
+            'W' => [-1, 0],
+        };
+
+        $newX = ($this->x + $mx + self::MAP_SIZE) % self::MAP_SIZE;
+        $newY = ($this->x + $my + self::MAP_SIZE) % self::MAP_SIZE;
+        if($this->isObstacle($newX, $newY)) {
+            $this->obstacleFound = true;
+            $this->stoppedPosition = [$this->x, $this->y];
+            return;
+        }
+        $this->x = $newX;
+        $this->y = $newY;
+
+    }
+
+    private function moveLeft() 
+    {
+
+    }
+
+    private function moveRight() 
+    {
+
+    }
+
+    private function isObstacle(int $x, int $y) 
+    {
+        foreach ($this->obstacles as [$ox, $oy]) {
+            if ($x === $ox && $y === $oy) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
