5b8bda6ad2b8de91fd775ca31f6893410320c54f
pmunne <munnepau@gmail.com>
2025-07-03 19:53:49 +0200

    docker error, migrated database and controller done and tested  with postman

--- backend/app/Http/Controllers/RoverController.php
+++ backend/app/Http/Controllers/RoverController.php
@@ -2,21 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illumante\Http\Request;
+use Illuminate\Http\Request;
 use App\Services\RoverService;
 
 
 class RoverController extends Controller 
 {
 
-    public function start() 
+    public function start(Request $request) 
     {
         
+        $validated = $this->validator($request);
+
+        $rover = new RoverService(
+            $validated['initial_position'], 
+            $validated['direction'], 
+            $validated['obstacles'] ?? []
+        );
+
+        $res  = $rover->execute($validated['commands']);    
+
+        return response()->json([$res]);
+
     }
 
-
-    private function validate() 
+    private function validator(Request $request): array 
     {
 
+        return $request->validate([
+            'initial_position' => 'required|array|size:2',
+            'initial_position.0' => 'required|integer',
+            'initial_position.1' => 'required|integer',
+            'direction' => 'required|string|in:N,E,S,W',
+            'obstacles' => 'nullable|array',
+            'commands' => 'required|string'
+
+        ]);
     }
 }
