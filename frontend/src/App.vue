f46a677ab5a4deb624b258bcd1eeef39b8b616e0
pmunne <munnepau@gmail.com>
2025-07-04 09:19:14 +0200

    html structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,65 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import RoverForm from './components/RoverForm.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="layout">
+    <header>
+        <h1>Notes</h1>
+    </header>
+    <main class="contaner">
+      <RoverForm />
+    </main>
+    <footer>
+      <p>Pau Munné Martínez</p>
+    </footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+<style>
+.layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: #f0f0f0;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.container {
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 0.5rem 1rem;
+    background-color: white;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+
+header {
+    background-color: rgba(0, 123, 255, 0.8);
+    color: white;
+    padding: 1.5rem 1rem;
+    text-align: center;
+}
+h1 {
+        font-size: 2rem;
+        font-weight: 600;
+        letter-spacing: 1rem;
+    }
+
+main {
+    flex: 1;
+    display: flex;
+    justify-content: center; 
+    padding: 2rem 1rem;
+    background-color: #f0f0f0;
+}
+
+
+footer {
+    background-color: rgba(0, 123, 255, 0.4);
+    color: white;
+    text-align: center;
+    padding: 1rem;
+    font-size: 0.9rem;
 }
 </style>
---
+++ frontend/src/components/RoverForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="rover-form">
+        <h2>Mars rover mission</h2>
+        <form>
+            <div>
+                <label>Initial position (x, y)</label>
+                <input type="number" placeholder="X" required>
+                <input type="number" placeholder="Y" required>
+            </div>
+            <div>
+                <label>Direction: </label>
+                <select required>
+                    <option value="N">North</option>
+                    <option value="E">East</option>
+                    <option value="S">South</option>
+                    <option value="W">West</option>
+                </select>
+            </div>
+            <div>
+                <label>Commands</label>
+                <input type="text" placeholder="frflf" required>
+            </div>
+            <div>
+                <label>Obstacles</label>
+                <input type="text" placeholder="2,2;5,5" required>
+            </div>
+            <button type="submit">Start mission</button>
+        </form>
+    </div>
+</template>
+<script setup>
+
+
+</script>
+
+<style scoped>
+.rover-form{
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 1rem;
+    border: 1px solid #ccc;
+}
+.rover-form input,
+.rover-form select {
+    margin: 0.5rem 0;
+    display: block;
+}
+
+</style>
